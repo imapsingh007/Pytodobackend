9125e98ea63f2db8fbe337cfda63d61f0588a491
imapsingh007 <150086761+imapsingh007@users.noreply.github.com>
2025-01-24 12:22:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, This is Backend application!"}
 
 if __name__ == "__main__":
     import uvicorn
