57d019cf6ca3d11a90d8729ee0beac62c44097b5
imapsingh007 <150086761+imapsingh007@users.noreply.github.com>
2025-01-24 12:15:00 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,5 @@
-# startup.sh
+# Install Python dependencies
+pip install -r requirements.txt
 
-# # Update system and install dependencies
-# apt-get update && apt-get install -y unixodbc unixodbc-dev
-# curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
-# apt-get update
-# ACCEPT_EULA=Y apt-get install -y msodbcsql17
-
-# Start the application
+# Start FastAPI application on port 80
 uvicorn app:app --host 0.0.0.0 --port 80
