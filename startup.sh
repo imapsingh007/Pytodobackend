bba53b08ec6b88f6742386c7c7e4721947ae81e5
imapsingh007 <150086761+imapsingh007@users.noreply.github.com>
2025-01-24 11:53:01 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 # startup.sh
 
-# Update system and install dependencies
-apt-get update && apt-get install -y unixodbc unixodbc-dev
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# # Update system and install dependencies
+# apt-get update && apt-get install -y unixodbc unixodbc-dev
+# curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# apt-get update
+# ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 # Start the application
 uvicorn main:app --host 0.0.0.0 --port 8000
